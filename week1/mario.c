f941c8d4b623b4019adf1e32bf6b9294cf8e75bc
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-05-02 02:55:58 +0000

    made mario look amazing

--- week1/mario.c
+++ week1/mario.c
@@ -11,43 +11,27 @@ int main(void)
         // height = get_int();
         height = get_int("Enter a number between 1 and 8\n");
     }
-    while (height < 0 || height > 8);
+    while (height < 1 || height > 8);
 
     // int spaces = height - 1;
-    for (int i = 0; i < height; i++)
+    // for loop base, which just iterates through and adds new line at end
+    for (int line = 0; line < height; line++)
     {
-        for (int spaces = height - i; spaces > 0; spaces--)
+        // spaces is equal to the number entered minus the line #, only when spaces is greater than 0
+        for (int spaces = height - line; spaces > 0; spaces--)
         {
-            printf (" ");
+            printf(" ");
             // printf("#");
         }
-        for (int hashes = -1; hashes < i; hashes ++)
+        // each line, hashes prints one more after it iterates through the spaces.
+        for (int hashes = -1; hashes < line; hashes ++)
         {
-            printf ("#");
+            printf("#");
             // spaces--;
             // printf(" ");
         }
+        // adds new line to each line
         printf("\n");
     }
-
 }
 
-
-
-    // int height = get_int("Enter a number between 1 and 8\n");
-    // while( height >= 9 && height < 0)
-    //     {
-    //     height = get_int("Enter a number between 1 and 8\n");
-    // //     if( height > 0 && height < 9) {
-    // //     printf("%i > 0 and %i < 9\n", height, height);
-    // // }
-    // // else {
-    // //     height = get_int("Enter a number between 1 and 8\n");
-    // // }
-    // }
-
-// prompt user for height
-//if height < 1 or height > 8 prompt user for height
-// height
-// height -1
-// height -2
