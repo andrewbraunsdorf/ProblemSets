62cfec6bd5e3ef5b1808d5bc4492292e07851ca0
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-05-16 23:48:05 +0000

    added mariomore

---
+++ week1/mariomore.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        // //Use this if they dont want the below code
+        // printf("Enter a number between 1 and 8\n");
+        // height = get_int();
+        height = get_int("Enter a number between 1 and 8\n");
+    }
+    while (height < 1 || height > 8);
+    // while (height < 1 && height > 8);
+    // while (height >= 1 && height <= 8);
+
+    // int spaces = height - 1;
+    // for loop base, which just iterates through and adds new line at end
+    for (int line = 0; line < height; line++)
+    {
+        // spaces is equal to the number entered minus the line #, only when spaces is greater than 0
+        for (int spaces = height - line - 1; spaces > 0; spaces--)
+        {
+            printf(" ");
+            // printf("#");
+        }
+        // each line, hashes prints one more after it iterates through the spaces.
+        for (int hashes = -1; hashes < line; hashes ++)
+        {
+            printf("#");
+            // spaces--;
+            // printf(" ");
+        }
+        printf("  ");
+        for (int hashes = -1; hashes < line; hashes ++)
+        {
+            printf("#");
+            // spaces--;
+            // printf(" ");
+        }
+        // adds new line to each line
+        printf("\n");
+    }
+}
+
